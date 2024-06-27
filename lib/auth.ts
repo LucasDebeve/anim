d5d24eb103eed919346b7bda8f5ddeee1c71d0c3
Lucas Debeve <lucas.debeve2003@gmail.com>
2024-06-27 23:01:28 +0200

    BD and Auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,11 @@
 import GithubProvider from "next-auth/providers/github"
 import {env} from "./env";
+import {AuthOptions} from "next-auth";
+import {PrismaAdapter} from "@next-auth/prisma-adapter";
+import {prisma} from "./prisma";
 
-export const authOptions = {
+export const authOptions : AuthOptions= {
+    adapter: PrismaAdapter(prisma),
     // Configure one or more authentication providers
     providers: [
         GithubProvider({
